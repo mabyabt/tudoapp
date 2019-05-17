8a82f3d95d2684df87895f50971ca68124716081
Matuz Bence <matuzb97@gmail.com>
2019-05-17 18:36:12 +0200

    init

---
+++ src/main/java/com/tudoapp/tudoapp/TudoappApplication.java
@@ -0,0 +1,13 @@
+package com.tudoapp.tudoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TudoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tudoapp/tudoapp/TudoappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tudoapp.tudoapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TudoappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
